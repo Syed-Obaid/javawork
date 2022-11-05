534a843d7e0d2eee83b5374ee6a542f372f1f475
Syed-Obaid <syedobaid932@gmail.com>
2022-11-05 03:56:28 -0700

    Project started

---
+++ app.js
@@ -0,0 +1,112 @@
+// var numberofcats = +prompt('How Many Cats');
+// var tomanycats = numberofcats + 1;
+
+
+// console.log(tomanycats);
+
+
+// var officer = true;
+// if(officer == true){
+//     console.log('Document submitted');
+// }else{
+//     console.log('Document not submitted');
+// }
+
+
+// var a = 10;
+// var b = 10;
+
+// if(a == b){
+//     console.log('A is Equal to B');
+// }
+
+
+// var a = 10;
+// var b = "10";
+
+// if(a === b){
+//     console.log('A is Equal to B');
+// }
+
+// var a = 10;
+// var b = 12;
+
+// if(a != b){
+//     console.log('A is Not Equal to B');
+// }
+
+
+// if(a < b){
+//     console.log('A is Less Than B')
+// }
+
+// if(a > b){
+//     console.log('A is Greater Than B')
+// }
+
+// if(a <= b){
+//     console.log('A is Less Than Equal to B')
+// }
+
+// if(a >= b){
+//     console.log('A is Greater Than Equal to B')
+// }
+
+// var officer = false;
+// var assistant = true;
+
+// if(officer == true){
+//     console.log('Document submitted')
+// }else if(assistant == true){
+//     console.log('Document submitted')
+// }else{
+//     console.log('not submitted')
+// }
+
+
+// var a = 10;
+// var b = 20;
+// var c = 5;
+
+// if(a < b && b > c){
+//     console.log('abc')
+// }
+
+// if(a < c || b > a){
+//     console.log('abc')
+// }
+
+
+// var age = 14;
+// var studentcard = true;
+
+// if(age == 18){
+//     console.log('Allow')
+// }else {
+//     if(studentcard ==true){
+//         console.log('Allow on studentcard')
+//     }else{
+//         console.log('not allow')
+//     }
+  
+// }
+
+
+// if(age == 18 || studentcard == true){
+//     console.log('allow')
+// }else{
+//     console.log('not allow')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
